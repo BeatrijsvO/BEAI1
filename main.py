e90cc32f64f7484543c1fa18efbbf0f78d4741a6
BeatrijsvO <beatrijs.van.oostveen@gmail.com>
2025-01-03 11:08:07 +0100

    middleware in main toegevoegd 1

--- main.py
+++ main.py
@@ -11,6 +11,15 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 # Definieer de FastAPI-app
 app = FastAPI()
 
+# Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
