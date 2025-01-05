66c2c26a52a07f9ace8ffd1d1e0fdafdbe0005dc
BeatrijsvO <beatrijs.van.oostveen@gmail.com>
2025-01-05 22:29:58 +0100

    nieuwe req

--- nlpgen/generation.py
+++ nlpgen/generation.py
@@ -1,15 +1,13 @@
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
-# Vervang BLOOMZ door Flan-T5
 tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-small")
 model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-small")
 
 def generate_answer(question: str, context: str) -> str:
     prompt = f"Context: {context}\n\nVraag: {question}\nAntwoord:"
     inputs = tokenizer(prompt, return_tensors="pt", max_length=512, truncation=True)
-    outputs = model.generate(inputs.input_ids, max_length=200, temperature=0.7, top_k=50)
+    outputs = model.generate(inputs.input_ids, max_length=150, temperature=0.7, top_k=50)
     return tokenizer.decode(outputs[0], skip_special_tokens=True)
-
 
 
 
